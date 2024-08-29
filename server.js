22f34fb5ea1c2bc7afdbf21c5a83aeab5c517420
HONEYKAYTECH <mariamoyinkansola11@yahoo.com>
2024-08-29 13:54:57 +0100

    update-blog-api

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-require('./config/dbConnect')
+const express = require("express");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/userRoutes");
+const postRouter = require("./routes/postRoutes");
+const commentRouter = require("./routes/commentRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
+const globalErrHandler = require("./middlewares/globalErrHandler");
+dotenv.config();
+require("./config/dbConnect");
+const app = express();
+
+//middleware
+app.use(express.json());
+const userAuth = {
+    isLogin: true,
+    isAdmin: false,
+};
+// app.use((req, res, next) => {
+//     if (userAuth.isLogin){
+//         next();
+//     }else{
+//         return res.json({
+//             msg: "Invalid login credentials",
+//         });
+//     }
+// }
+
+// )
+//routes
+
+//----User---
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/post",postRouter);
+app.use("/api/v1/comment",commentRouter)
+app.use("/api/v1/category",categoryRouter)
 
 
+//Error handler middleware
+app.use(globalErrHandler);
 
-
-
-
-
-
-
-
-
+//404 error
+app.use("*", (req ,res) => {
+    res.status(404).json({
+        message: `${req.originalUrl} - Route not found`,
+    });
+});
+//Listen to server
 
 
 
